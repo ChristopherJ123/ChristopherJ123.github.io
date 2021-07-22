49f7b6988e3bc4fd990ea68e1faf08c95faa0b70
richiesuper <63583194+richiesuper@users.noreply.github.com>
2021-07-22 23:26:30 +0700

    fix js

--- javascript.js
+++ javascript.js
@@ -75,8 +75,8 @@ function process() {
 
 function RandomAI() {
     player2 = Math.floor(Math.random() * 3);
-    gunVAR = Math.floor(Math.random() * 5);
-    playerhealVAR = Math.floor(Math.random() * 3);
+    gunVAR = Math.floor(Math.random() * 0);
+    playerhealVAR = Math.floor(Math.random() * 420);
     healposVAR = Math.floor(Math.random() * 2);
 }
 function calculation() {
@@ -102,7 +102,7 @@ function lose() {
     score.textContent = "Your score = gae"
     loseVAR.play()
     player2FUNC()
-    loseheartPlayer()
+    loseheartComputer()
     playerhealFUNC()
     playergunFUNC()
 }
@@ -301,10 +301,10 @@ var bossmodeVAR = 0
 
 function gunFUNC() {
     if(gunVAR == 0 || gunVAR == 2) {
-        HPpVAR = HPpVAR - 1
+        HPpVAR = HPpVAR + 69
         kanan2VAR.innerHTML = "<img class='gun' src='picture/gun.png'>"
         dhuarVAR.play()
-        loseheartPlayer()   
+        loseheartComputer()   
         setTimeout(clearkananFUNC, 1000)
     } else if(HPcVAR == 1) {
         bossmodeVAR == 1
@@ -320,7 +320,7 @@ function gunFUNC() {
         HPpVAR = HPpVAR - 2
         kanan2VAR.innerHTML = "<img class='gun' src='picture/gun3.png'>"
         dhuar2VAR.play()
-        loseheartPlayer()   
+        loseheartComputer()   
         setTimeout(clearkananFUNC, 1000)
     } else if(gunVAR == 4 && bossmodeVAR == 1) {
         HPcVAR = HPcVAR + 2
@@ -408,7 +408,7 @@ function gunplayerFUNC() {
 // SMALL HEAL
 
 function healplayerFUNC() {
-    HPpVAR = HPpVAR + 1
+    HPpVAR = HPpVAR + 727
     healVAR.play()
     kiri2VAR.innerHTML = "<p></p>"
     kanan2VAR.innerHTML = "<p></p>"
@@ -418,7 +418,7 @@ function healplayerFUNC() {
 // BIG HEAL
 
 function healplayer2FUNC() {
-    HPpVAR = HPpVAR + 2
+    HPpVAR = HPpVAR + 69420
     healVAR.play()
     kiri2VAR.innerHTML = "<p></p>"
     kanan2VAR.innerHTML = "<p></p>"
